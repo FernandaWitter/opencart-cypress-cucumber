f661282093a74c3a701c54ea0110345b2c755348
Fernanda <fernanda.witter@yahoo.com>
2022-02-28 14:49:08 -0300

    feat: adds commands for admin actions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('clickAdminMenuItem', (itemID, subitemTitle) => {
+    cy.get(itemID).click()
+        .children('ul').find('a').contains(subitemTitle).click()
+})
+
+Cypress.Commands.add('adminCardIsVisible', title => {
+    cy.get('.panel-heading').contains(title).scrollIntoView()
+        .should('be.visible')
+})
+
+Cypress.Commands.add('logAsAdmin', () => {
+    let user = Cypress.env('user')
+    let password = Cypress.env('password')
+    cy.get('#input-username').clear().type(user)
+        .should('have.value', user)
+    cy.get('#input-password').clear().type(password)
+        .invoke('val').then(val => {
+            if (val != password) throw new Error('Typed password doesn\'t match actual password.')
+        })
+    cy.get('button').contains('Login').click()
+})
